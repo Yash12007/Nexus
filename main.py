8b6b82f69034b080c28f40ed57c145f9361419b8
Yash Bodhe <106073201+Yash12007@users.noreply.github.com>
2023-08-16 14:29:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import webview
+
+webview.create_window(
+    "Nexus OS",
+    url='https://yash12007.github.io/Nexus/',
+    width=1280,
+    height=720,
+    confirm_close=True
+)
+webview.start()
